484f9b37da9c01b2c72e31d996cb127145c61a5e
ZonaLimite <javier.boga@yahoo.es>
2026-04-29 17:47:34 +0200

    Ultimas limpias

--- src/main/java/cta/remote/service/RemoteEngine.java
+++ src/main/java/cta/remote/service/RemoteEngine.java
@@ -20,10 +20,10 @@ import java.util.Vector;
 
 @Component
 public class RemoteEngine {
-	
+
 	@Autowired
 	private Visualizador vis;
-	
+
 	public void SelectSistema(String sistema) {
 		vis.getComboSistemas().setSelectedItem(sistema);
 	}
@@ -31,78 +31,77 @@ public class RemoteEngine {
 	public void bConectar() {
 		vis.getConectar().doClick();
 	}
-	
+
 	public void bDesconectar() {
 		vis.getDesconectar().doClick();
 	}
-	
+
 	public void adjustNumTop(int num) {
 		vis.spinner.setValue(num);
 	}
-	
+
 	public void incluirModelFilterAListener(String nameModelFilter) {
 		vis.incluirModelFilterAListenersRemote(nameModelFilter);
 	}
-	
+
 	public void borrarModelFilterDeListenerActivos(String nameModelFilterActivo) {
 		ModelFilter modelFilter = vis.getCatalogoModelFilters().get(nameModelFilterActivo);
 		vis.borrarModelFilterDeListener(modelFilter);
 	}
-	
-	//Ojo la clave es compuesta con sistema:nameconsulta
+
+	// Ojo la clave es compuesta con sistema:nameconsulta
 	public void selectConsulta(String nameSistemaConsulta) {
-		 vis.getCombo_Consultas().setSelectedItem( vis.getCatalogoConsultas().get(nameSistemaConsulta));
-		 
+		vis.getCombo_Consultas().setSelectedItem(vis.getCatalogoConsultas().get(nameSistemaConsulta));
+
 	}
-	
+
 	public String[] getSistemasRegistrados() {
 		return vis.aSistemas;
 	}
-	
+
 	public String[] getModulosRegistrados(String pareEsteSistema) {
 		ConcurrentHashMap<String, Consulta> catConsultas = vis.getCatalogoConsultas();
 		Vector<String> v = new Vector<String>();
 		Set<Entry<String, Consulta>> set = catConsultas.entrySet();
-		
-		Iterator<Entry<String,Consulta>> it = set.iterator();
-		while(it.hasNext()) {
+
+		Iterator<Entry<String, Consulta>> it = set.iterator();
+		while (it.hasNext()) {
 			Entry<String, Consulta> e = it.next();
-			if(e.getKey().contains(pareEsteSistema+":")){
+			if (e.getKey().contains(pareEsteSistema + ":")) {
 				v.add(e.getValue().getNombreConsultaFull());
 			}
-		};
+		}
+		;
 		return v.toArray(new String[v.size()]);
 	}
 
 	public String[] getListenersRegistrados() {
-		Vector<String> vKeys =new Vector<String>();
-		for ( Enumeration<String> enumKeys = vis.getCatalogoModelFilters().keys();enumKeys.hasMoreElements();) {
-			   vKeys.add(enumKeys.nextElement());
+		Vector<String> vKeys = new Vector<String>();
+		for (Enumeration<String> enumKeys = vis.getCatalogoModelFilters().keys(); enumKeys.hasMoreElements();) {
+			vKeys.add(enumKeys.nextElement());
 		}
 		return vKeys.toArray(new String[vKeys.size()]);
-		
-	}
-	
-	public String[] getListenersActivos() {
-		return vis.getListenersActivos();
-		
+
 	}
 
-	
+	public String[] getListenersActivos() {
+		return vis.getListenersActivos();
+
+	}
+
 	public void setTextListenerRapido(String text) {
 		vis.getTextFieldListener1().setText(text);
-		
+
 	}
-	
+
 	public void doClickButtonListenerRapido() {
 		vis.btnIncluirListenerRapido.doClick();
 
 	}
-	
+
 	public void doClickButtonPublishToSocket(boolean status) {
-		//remoteControl
+		// remoteControl
 		vis.chckbxPublishToWebsocket.setSelected(status);
 	}
-	
-	
+
 }
--- src/main/java/cta/remote/stompbroker/EngineProtocol.java
+++ src/main/java/cta/remote/stompbroker/EngineProtocol.java
@@ -1,10 +1,7 @@
 package cta.remote.stompbroker;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
-
-import javax.swing.ComboBoxModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -12,164 +9,167 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import cta.designe.listener.ModelFilter;
 import cta.remote.service.RemoteEngine;
 
 @Controller
 public class EngineProtocol {
-	
+
 	@Autowired
 	private SimpMessagingTemplate webSocket;
-	
+
 	@Autowired
 	private RemoteEngine re;
-	
-	private String[] colores = {"red", "green", "blue", "magenta", "purple", "orange"};
-	
+
+	private String[] colores = { "red", "green", "blue", "magenta", "purple", "orange" };
+
 	@MessageMapping("/manage_engine")
-	//@SendTo("/channel/control") //De momento no hay necesidad de brokear
+	// @SendTo("/channel/control") //De momento no hay necesidad de brokear
 	public ModelResultData recibeMensaje(ModelResultData comando) {
-		ModelResultData rd=null;
-		
-		/*String tipoResult;
-		  String[] data;*/
-		System.out.println("Comando recibido :"+ comando.getTipoResult() +"-->"+( comando.data.length==0?"":comando.data[0]));
+		ModelResultData rd = null;
+
+		/*
+		 * String tipoResult;
+		 * String[] data;
+		 */
+		System.out.println("Comando recibido :" + comando.getTipoResult() + "-->"
+				+ (comando.data.length == 0 ? "" : comando.data[0]));
 		switch (comando.tipoResult) {
-		
-		case "maquinas":
-			//accion sobre engine
 
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="maquinas";
-			rd.data = new String[]{"1","2"};
-			this.enviarDirecto(rd);
-			break;
-			
-		case "sistemas":
-			//accion sobre engine
+			case "maquinas":
+				// accion sobre engine
 
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="sistemas";
-			rd.data = re.getSistemasRegistrados();
-			this.enviarDirecto(rd);
-			break;
-			
-		case "modulos":	
-			//accion sobre engine
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "maquinas";
+				rd.data = new String[] { "1", "2" };
+				this.enviarDirecto(rd);
+				break;
 
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="modulos";
-			rd.data = re.getModulosRegistrados(comando.data[0]);
-			this.enviarDirecto(rd);
-			break;
+			case "sistemas":
+				// accion sobre engine
 
-		case "listeners":	
-			//accion sobre engine
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "sistemas";
+				rd.data = re.getSistemasRegistrados();
+				this.enviarDirecto(rd);
+				break;
 
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="listeners";
-			rd.data = re.getListenersRegistrados();
-			this.enviarDirecto(rd);
-			break;
-			
-		case "listenersActivos":	
-			//accion sobre engine
+			case "modulos":
+				// accion sobre engine
 
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="listenersActivos";
-			rd.data = re.getListenersActivos();
-			this.enviarDirecto(rd);
-			break;
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "modulos";
+				rd.data = re.getModulosRegistrados(comando.data[0]);
+				this.enviarDirecto(rd);
+				break;
 
-		case "selectSistema":
-			//accion sobre engine
-			re.SelectSistema(comando.data[0]);
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="sistemas";
-			rd.data = re.getSistemasRegistrados();
-			break;	
-		case "conectar":
-			//accion sobre engine
-			re.bConectar();
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="ackConectar";
-			rd.data = new String[1];
-			this.enviarDirecto(rd);
+			case "listeners":
+				// accion sobre engine
 
-			break;
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "listeners";
+				rd.data = re.getListenersRegistrados();
+				this.enviarDirecto(rd);
+				break;
 
-		case "desconectar":
-			//accion sobre engine
-			re.bDesconectar();
-			//callback 
-			rd = new ModelResultData();
-			rd.tipoResult="ackDesconectar";
-			rd.data = new String[1];
-			this.enviarDirecto(rd);
-			break;
-		
-		case "incluirListenerRapido":
-			//accion sobre engine
-			re.setTextListenerRapido(comando.data[0]);
-			break;	
-			
-		case "doClickListenerrapido":	
-			re.doClickButtonListenerRapido();
-	
-			break;
-		case "adjustnumtop":
-			re.adjustNumTop(Integer.parseInt(comando.data[0]));
-			// pendiente remitir ackknowlegde
-			break;
+			case "listenersActivos":
+				// accion sobre engine
 
-		case "incluirlistenermodelfilter":
-			re.incluirModelFilterAListener(comando.data[0]);
-			// devolver modelfiltersActivos
-			break;
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "listenersActivos";
+				rd.data = re.getListenersActivos();
+				this.enviarDirecto(rd);
+				break;
 
-		case "quitarlistenermodelfilter":
-			re.borrarModelFilterDeListenerActivos(comando.data[0]);
-			// devolver modelfiltersActivos
-			break;
-		case "selectConsulta":
-			re.selectConsulta(comando.data[0]);
-			//devolver AckKnowledge siempre como Response Entity
-			break;
-		case "setWebsocketPublish":
-			String status = comando.data[0];
-			if(status.equals("1"))re.doClickButtonPublishToSocket(true);
-			if(status.equals("0"))re.doClickButtonPublishToSocket(false);
-			break;
-		}	
+			case "selectSistema":
+				// accion sobre engine
+				re.SelectSistema(comando.data[0]);
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "sistemas";
+				rd.data = re.getSistemasRegistrados();
+				break;
+			case "conectar":
+				// accion sobre engine
+				re.bConectar();
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "ackConectar";
+				rd.data = new String[1];
+				this.enviarDirecto(rd);
+
+				break;
+
+			case "desconectar":
+				// accion sobre engine
+				re.bDesconectar();
+				// callback
+				rd = new ModelResultData();
+				rd.tipoResult = "ackDesconectar";
+				rd.data = new String[1];
+				this.enviarDirecto(rd);
+				break;
+
+			case "incluirListenerRapido":
+				// accion sobre engine
+				re.setTextListenerRapido(comando.data[0]);
+				break;
+
+			case "doClickListenerrapido":
+				re.doClickButtonListenerRapido();
+
+				break;
+			case "adjustnumtop":
+				re.adjustNumTop(Integer.parseInt(comando.data[0]));
+				// pendiente remitir ackknowlegde
+				break;
+
+			case "incluirlistenermodelfilter":
+				re.incluirModelFilterAListener(comando.data[0]);
+				// devolver modelfiltersActivos
+				break;
+
+			case "quitarlistenermodelfilter":
+				re.borrarModelFilterDeListenerActivos(comando.data[0]);
+				// devolver modelfiltersActivos
+				break;
+			case "selectConsulta":
+				re.selectConsulta(comando.data[0]);
+				// devolver AckKnowledge siempre como Response Entity
+				break;
+			case "setWebsocketPublish":
+				String status = comando.data[0];
+				if (status.equals("1"))
+					re.doClickButtonPublishToSocket(true);
+				if (status.equals("0"))
+					re.doClickButtonPublishToSocket(false);
+				break;
+		}
 		return rd;
-		
+
 	}
-	
+
 	public void enviarDirecto(ModelResultData mrd) {
-		webSocket.convertAndSend("/channel/control", mrd);	
-		System.out.println("Tratando de enviar a cliente:"+mrd);
+		webSocket.convertAndSend("/channel/control", mrd);
+		System.out.println("Tratando de enviar a cliente:" + mrd);
 	}
-	
-	
+
 	@MessageMapping("/mensaje")
 	@SendTo("/channel/mensaje")
 	public ModelMensaje recibeMensaje(ModelMensaje mensaje) {
 		mensaje.setFecha(new Date().getTime());
-		
-		if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
+
+		if (mensaje.getTipo().equals("NUEVO_USUARIO")) {
 			mensaje.setColor(colores[new Random().nextInt(colores.length)]);
 			mensaje.setTexto("nuevo usuario");
 		} else {
-			//chatService.guardar(mensaje);
+			// chatService.guardar(mensaje);
 		}
-		
+
 		return mensaje;
 	}
 
@@ -178,7 +178,5 @@ public class EngineProtocol {
 	public String estaEscribiendo(String username) {
 		return username.concat(" está escribiendo ...");
 	}
-	
-	
-	
+
 }
